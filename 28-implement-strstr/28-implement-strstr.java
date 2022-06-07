49bf410d13fa1a1d376e541bf51867b5374f7d97
Mohamed Saad-Eldeen <45404445+mohamedsaaad@users.noreply.github.com>
2022-06-07 19:16:34 +0200

    Time: 1 ms (58.43%), Space: 42.1 MB (34.05%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,7 +1,14 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-            return haystack.indexOf(needle);
-        
-        
+        if(needle.isEmpty() || needle.equals(haystack))
+            return 0;
+        int limit=haystack.length()- needle.length();
+        int windo=needle.length();
+        for (int i = 0; i <= limit ; i++) {
+            String subString=haystack.substring(i,i+windo);
+            if(needle.equals(subString))
+                return i;
+        }
+        return -1;  
     }
 }

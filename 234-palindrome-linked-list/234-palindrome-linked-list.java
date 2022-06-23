bebeb31a896c2f3e8765d55dc9afd62b278f9694
Mohamed Saad-Eldeen <45404445+mohamedsaaad@users.noreply.github.com>
2022-06-23 21:27:07 +0200

    Time: 24 ms (22.20%), Space: 103.2 MB (33.30%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,26 +9,26 @@
  * }
  */
 class Solution {
-//     ListNode ref;
-//     public boolean isPalindrome(ListNode head) {
-//         ref=head;
+    ListNode ref;
+    public boolean isPalindrome(ListNode head) {
+        ref=head;
         
-//         return check(head);
-//     }
+        return check(head);
+    }
     
-//     public boolean check(ListNode node){
-//         if(node==null) return true;
-//         boolean ans=check(node.next);//1- true from base case last node{1} ->
-//                                      //2- true from last case the node prev the last{2}
-//         boolean checkSimilarity=(ref.val==node.val)?true:false;
-//         ref=ref.next;
-//         return ans && checkSimilarity;
+    public boolean check(ListNode node){
+        if(node==null) return true;
+        boolean ans=check(node.next);//1- true from base case last node{1} ->
+                                     //2- true from last case the node prev the last{2}
+        boolean checkSimilarity=(ref.val==node.val)?true:false;
+        ref=ref.next;
+        return ans && checkSimilarity;
         
-//     }
+    }
     
     
     
-    public boolean isPalindrome(ListNode head){
+    public boolean isPalindromeWithFloydDetectCycleAlgo(ListNode head){
     //use floyd cycle detection algorithm
         ListNode fast=head;
         ListNode slow=head;
